c2997557cd0a10bee8935f71f984a2ac43721926
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2023-07-29 09:47:59 +0700

    Update componets folder

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,6 +1,20 @@
 import React, { Fragment } from 'react';
 const Popup = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      {/* Navbar */}
+      <div className="bg-red-400 flex justify-between py-4">
+        <button>Hamburger</button>
+      </div>
+
+      {/* Language bar */}
+      <div className="bg-orange-400 flex px-[40px] py-4 justify-between">
+        <div>English</div>
+        <div>to</div>
+        <div>Janpanese</div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Popup;
