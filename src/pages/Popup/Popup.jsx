45ddd0d61744838b41c960096b50989bdf6eeff5
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2023-07-29 10:09:38 +0700

    Modify homepapge for Popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { HiOutlineSwitchHorizontal } from 'react-icons/hi';
 const Popup = () => {
   return (
     <Fragment>
@@ -8,10 +9,26 @@ const Popup = () => {
       </div>
 
       {/* Language bar */}
-      <div className="bg-orange-400 flex px-[40px] py-4 justify-between">
-        <div>English</div>
-        <div>to</div>
-        <div>Janpanese</div>
+      <div className="bg-orange-400 flex px-[40px] py-4 justify-between items-center">
+        <button className="bg-red-200 p-2 rounded-2xl px-4">Automatic</button>
+        <button className="flex items-center justify-center bg-red-400 rounded-full p-2">
+          <HiOutlineSwitchHorizontal />
+        </button>
+        <button className="bg-red-200 p-2 rounded-2xl px-4">Vietnamese</button>
+      </div>
+
+      {/* Translate Boxes */}
+      <div>
+        <textarea name="" className="bg-red-200" id="" cols="100" rows="10">
+          Hello
+        </textarea>
+        <textarea
+          name=""
+          className="bg-red-200"
+          id=""
+          cols="100"
+          rows="10"
+        ></textarea>
       </div>
     </Fragment>
   );
